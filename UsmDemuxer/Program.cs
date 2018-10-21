9f72b02d13a9f1fef0ca40189b3b3bf0cbbfe9bb
ester <ester@ESTER-PC>
2018-10-21 11:24:51 +0800

    添加项目文件。

---
+++ UsmDemuxer/Program.cs
@@ -0,0 +1,21 @@
+﻿using VGMToolbox.format;
+
+namespace UsmDemuxer
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length < 1) return 1;
+            string path = args[0];
+            new CriUsmStream(path).DemultiplexStreams(new MpegStream.DemuxOptionsStruct
+            {
+                ExtractAudio = true,
+                ExtractVideo = true,
+                AddHeader = false,
+                SplitAudioStreams = false
+            });
+            return 0;
+        }
+    }
+}
